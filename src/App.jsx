53b503511ef14a507093e7d70212c94174ac279e
bkdhanush <bkdhanushofficial@gmail.com>
2024-03-21 18:35:09 +0530

    update:Create basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useCallback } from 'react';
-import ReactFlow, { useNodesState, useEdgesState, addEdge, MiniMap, Controls } from 'reactflow';
+import ReactFlow, { useNodesState, useEdgesState, addEdge, MiniMap, Controls, MarkerType } from 'reactflow';
 
 import 'reactflow/dist/base.css';
 import CustomNode from './CustomNode';
@@ -12,27 +12,147 @@ const initNodes = [
   {
     id: '1',
     type: 'custom',
-    data: { name: 'Jane Doe', job: 'CEO', emoji: '😎' },
+    data: { name: 'start', bgcolor:'#FFC2C2' },
     sourcePosition: 'right',
-    targetPosition: 'left',
-    position: { x: 0, y: 50 },
+    position: { x: 0, y: 400 },
   },
   {
     id: '2',
     type: 'custom',
-    data: { name: 'Tyler Weary', job: 'Designer', emoji: '🤓' },
+    data: { name: 'Research', bgcolor:'#FFC2C2' },
     sourcePosition: 'right',
     targetPosition: 'left',
-    position: { x: -200, y: 200 },
+    position: { x: 200, y: 0 },
   },
   {
     id: '3',
     type: 'custom',
-    data: { name: 'Kristi Price', job: 'Developer', emoji: '🤩' },
+    data: { name: 'Planning', bgcolor:'#FFC2C2' },
     sourcePosition: 'right',
     targetPosition: 'left',
     position: { x: 200, y: 200 },
   },
+  {
+    id: '4',
+    type: 'custom',
+    data: { name: 'Designing', bgcolor:'#FFC2C2' },
+    sourcePosition: 'right',
+    targetPosition: 'left',
+    position: { x: 200, y: 400 },
+  },
+  {
+    id: '5',
+    type: 'custom',
+    data: { name: 'Manufacturing', bgcolor:'#FFC2C2' },
+    sourcePosition: 'right',
+    targetPosition: 'left',
+    position: { x: 200, y: 600 },
+  },
+  {
+    id: '6',
+    type: 'custom',
+    data: { name: 'Sales/Marketing', bgcolor:'#FFC2C2' },
+    sourcePosition: 'right',
+    targetPosition: 'left',
+    position: { x: 200, y: 800 },
+  },
+  {
+    id: '7',
+    type: 'custom',
+    data: { name: 'External', bgcolor:'#FFC2C2' },
+    sourcePosition: 'right',
+    targetPosition: 'left',
+    position: { x: 600, y: -50 },
+  },
+  {
+    id: '8',
+    type: 'custom',
+    data: { name: 'Inernal', bgcolor:'#FFC2C2' },
+    sourcePosition: 'right',
+    targetPosition: 'left',
+    position: { x: 600, y: 50 },
+  },
+  {
+    id: '9',
+    type: 'custom',
+    data: { name: 'PRD', bgcolor:'#FFC2C2' },
+    sourcePosition: 'right',
+    targetPosition: 'left',
+    position: { x: 600, y: 150 },
+  },
+  {
+    id: '10',
+    type: 'custom',
+    data: { name: 'Specs', bgcolor:'#FFC2C2' },
+    sourcePosition: 'right',
+    targetPosition: 'left',
+    position: { x: 600, y: 250 },
+  },
+  {
+    id: '11',
+    type: 'custom',
+    data: { name: 'Hardware', bgcolor:'#FFC2C2' },
+    sourcePosition: 'right',
+    targetPosition: 'left',
+    position: { x: 600, y: 350 },
+  },
+  {
+    id: '12',
+    type: 'custom',
+    data: { name: 'Software', bgcolor:'#FFC2C2' },
+    sourcePosition: 'right',
+    targetPosition: 'left',
+    position: { x: 600, y: 450 },
+  },
+  {
+    id: '13',
+    type: 'custom',
+    data: { name: 'Material', bgcolor:'#FFC2C2' },
+    sourcePosition: 'right',
+    targetPosition: 'left',
+    position: { x: 600, y: 550 },
+  },
+  {
+    id: '14',
+    type: 'custom',
+    data: { name: 'Production', bgcolor:'#FFC2C2' },
+    sourcePosition: 'right',
+    targetPosition: 'left',
+    position: { x: 600, y: 650 },
+  },
+  {
+    id: '15',
+    type: 'custom',
+    data: { name: 'Online', bgcolor:'#FFC2C2' },
+    sourcePosition: 'right',
+    targetPosition: 'left',
+    position: { x: 600, y: 750 },
+  },
+  {
+    id: '16',
+    type: 'custom',
+    data: { name: 'Dealership', bgcolor:'#FFC2C2' },
+    sourcePosition: 'right',
+    targetPosition: 'left',
+    position: { x: 600, y: 850 },
+  },
+  {
+    id: '17',
+    type: 'custom',
+    data: { name: 'B2C', bgcolor:'#FFC2C2' },
+    sourcePosition: 'right',
+    targetPosition: 'left',
+    position: { x: 1000, y: -100 },
+  },
+  {
+    id: '18',
+    type: 'custom',
+    data: { name: 'B2C', bgcolor:'#FFC2C2' },
+    sourcePosition: 'right',
+    targetPosition: 'left',
+    position: { x: 1000, y: 0 },
+  },
+  
 ];
 
 const initEdges = [
@@ -40,11 +160,154 @@ const initEdges = [
     id: 'e1-2',
     source: '1',
     target: '2',
+    type: 'smoothstep',
+    markerEnd: {
+      type: MarkerType.ArrowClosed,
+    },
   },
   {
-    id: 'e2-3',
-    source: '2',
+    id: 'e1-3',
+    source: '1',
     target: '3',
+    type: 'smoothstep',
+    markerEnd: {
+      type: MarkerType.ArrowClosed,
+    },
+  },
+  {
+    id: 'e1-4',
+    source: '1',
+    target: '4',
+    type: 'smoothstep',
+    markerEnd: {
+      type: MarkerType.ArrowClosed,
+    },
+  },
+  {
+    id: 'e1-5',
+    source: '1',
+    target: '5',
+    type: 'smoothstep',
+    markerEnd: {
+      type: MarkerType.ArrowClosed,
+    },
+  },
+  {
+    id: 'e1-6',
+    source: '1',
+    target: '6',
+    type: 'smoothstep',
+    markerEnd: {
+      type: MarkerType.ArrowClosed,
+    },
+  },
+  {
+    id: 'e2-7',
+    source: '2',
+    target: '7',
+    type: 'smoothstep',
+    markerEnd: {
+      type: MarkerType.ArrowClosed,
+    },
+  },
+  {
+    id: 'e2-8',
+    source: '2',
+    target: '8',
+    type: 'smoothstep',
+    markerEnd: {
+      type: MarkerType.ArrowClosed,
+    },
+  },
+  {
+    id: 'e3-9',
+    source: '3',
+    target: '9',
+    type: 'smoothstep',
+    markerEnd: {
+      type: MarkerType.ArrowClosed,
+    },
+  },
+  {
+    id: 'e3-10',
+    source: '3',
+    target: '10',
+    type: 'smoothstep',
+    markerEnd: {
+      type: MarkerType.ArrowClosed,
+    },
+  },
+  {
+    id: 'e4-11',
+    source: '4',
+    target: '11',
+    type: 'smoothstep',
+    markerEnd: {
+      type: MarkerType.ArrowClosed,
+    },
+  },
+  {
+    id: 'e4-12',
+    source: '4',
+    target: '12',
+    type: 'smoothstep',
+    markerEnd: {
+      type: MarkerType.ArrowClosed,
+    },
+  },
+  {
+    id: 'e5-13',
+    source: '5',
+    target: '13',
+    type: 'smoothstep',
+    markerEnd: {
+      type: MarkerType.ArrowClosed,
+    },
+  },
+  {
+    id: 'e5-14',
+    source: '5',
+    target: '14',
+    type: 'smoothstep',
+    markerEnd: {
+      type: MarkerType.ArrowClosed,
+    },
+  },
+  {
+    id: 'e6-15',
+    source: '6',
+    target: '15',
+    type: 'smoothstep',
+    markerEnd: {
+      type: MarkerType.ArrowClosed,
+    },
+  },
+  {
+    id: 'e6-16',
+    source: '6',
+    target: '16',
+    type: 'smoothstep',
+    markerEnd: {
+      type: MarkerType.ArrowClosed,
+    },
+  },
+  {
+    id: 'e7-17',
+    source: '7',
+    target: '17',
+    type: 'smoothstep',
+    markerEnd: {
+      type: MarkerType.ArrowClosed,
+    },
+  },
+  {
+    id: 'e7-18',
+    source: '7',
+    target: '18',
+    type: 'smoothstep',
+    markerEnd: {
+      type: MarkerType.ArrowClosed,
+    },
   },
 ];
 
@@ -52,8 +315,19 @@ const Flow = () => {
   const [nodes, setNodes, onNodesChange] = useNodesState(initNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initEdges);
 
-  const onConnect = useCallback((params) => setEdges((eds) => addEdge(params, eds)), []);
-
+  const onConnect = useCallback((params) => {
+    const edge = {
+      ...params,
+      type: 'smoothstep',
+        markerEnd: {
+          type: MarkerType.ArrowClosed,
+      },
+      
+    };
+    setEdges((eds) => addEdge(edge, eds));
+  }, []);
+  
+  
   return (
     <div className='w-[100vw] h-[100vh]'>
     <ReactFlow
--- src/CustomNode.jsx
+++ src/CustomNode.jsx
@@ -1,29 +1,29 @@
-import React, { memo, useState } from 'react';
-import { Handle, Position } from 'reactflow';
+import React, { memo, useState } from "react";
+import { Handle, Position } from "reactflow";
 
 function CustomNode({ data }) {
-  const [ isShown, setIsShown ] = useState(null);
+  const [isShown, setIsShown] = useState(null);
   return (
-<div className='relative'>
-{isShown && <p className='text-lg absolute top-[-30px]'>Hello</p>}
-    <div className="px-4 py-2 shadow-md rounded-md bg-white border-2 border-stone-400" onMouseEnter={() => setIsShown(data.name)}
-    onMouseLeave={() => setIsShown(null)}>
-      <div className="flex">
-        <div className="rounded-full w-12 h-12 flex justify-center items-center bg-gray-100">
-          {data.emoji}
-        </div>
-        <div className="ml-2">
-          <div className="text-lg font-bold">{data.name}</div>
-          <div className="text-gray-500">{data.job}</div>
-        </div>
-        
+    <div className="relative">
+      {isShown && <p className="text-lg absolute top-[-30px]">Hello</p>}
+      <div
+        className="px-4 py-2 shadow-md rounded-md bg-white border-2 border-stone-400"
+        style={{backgroundColor:data.bgcolor}}
+        onMouseEnter={() => setIsShown(data.name)}
+        onMouseLeave={() => setIsShown(null)}
+      >
+        <div className="text-lg font-bold">{data.name}</div>
+        <Handle
+          type="target"
+          position={Position.Left}
+          className={`w-2 h-2 !bg-teal-500 rounded-full ${!isShown && "invisible"}`}
+        />
+        <Handle
+          type="source"
+          position={Position.Right}
+          className={`w-2 h-2 !bg-teal-500 rounded-full ${!isShown && "invisible"}`}
+        />
       </div>
-      
-      <Handle type="target" position={Position.Left} className="w-2 h-2 !bg-teal-500 rounded-full" />
-      <Handle type="source" position={Position.Right} className="w-2 h-2 !bg-teal-500 rounded-full" />
-      
-    </div>
-    
     </div>
   );
 }
