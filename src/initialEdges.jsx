07764ff9d12ea36389621291ac7c9b10f1e87aee
bkdhanush <bkdhanushofficial@gmail.com>
2024-03-22 14:18:29 +0530

    update: Add cloudinary + Input popup

---
+++ src/initialEdges.jsx
@@ -0,0 +1,192 @@
+// Initial Edge Variables
+import { MarkerType } from "reactflow";
+
+export const initEdges = [
+  {
+    id: "e1-2",
+    source: "1",
+    target: "2",
+    type: "smoothstep",
+    style: { stroke: "black" },
+    markerEnd: {
+      type: MarkerType.ArrowClosed,
+      color: "black",
+    },
+  },
+  {
+    id: "e1-3",
+    source: "1",
+    target: "3",
+    type: "smoothstep",
+    style: { stroke: "black" },
+    markerEnd: {
+      type: MarkerType.ArrowClosed,
+      color: "black",
+    },
+  },
+  {
+    id: "e1-4",
+    source: "1",
+    target: "4",
+    type: "smoothstep",
+    style: { stroke: "black" },
+    markerEnd: {
+      type: MarkerType.ArrowClosed,
+      color: "black",
+    },
+  },
+  {
+    id: "e1-5",
+    source: "1",
+    target: "5",
+    type: "smoothstep",
+    style: { stroke: "black" },
+    markerEnd: {
+      type: MarkerType.ArrowClosed,
+      color: "black",
+    },
+  },
+  {
+    id: "e1-6",
+    source: "1",
+    target: "6",
+    type: "smoothstep",
+    style: { stroke: "black" },
+    markerEnd: {
+      type: MarkerType.ArrowClosed,
+      color: "black",
+    },
+  },
+  {
+    id: "e2-7",
+    source: "2",
+    target: "7",
+    type: "smoothstep",
+    style: { stroke: "black" },
+    markerEnd: {
+      type: MarkerType.ArrowClosed,
+      color: "black",
+    },
+  },
+  {
+    id: "e2-8",
+    source: "2",
+    target: "8",
+    type: "smoothstep",
+    style: { stroke: "black" },
+    markerEnd: {
+      type: MarkerType.ArrowClosed,
+      color: "black",
+    },
+  },
+  {
+    id: "e3-9",
+    source: "3",
+    target: "9",
+    type: "smoothstep",
+    style: { stroke: "black" },
+    markerEnd: {
+      type: MarkerType.ArrowClosed,
+      color: "black",
+    },
+  },
+  {
+    id: "e3-10",
+    source: "3",
+    target: "10",
+    type: "smoothstep",
+    style: { stroke: "black" },
+    markerEnd: {
+      type: MarkerType.ArrowClosed,
+      color: "black",
+    },
+  },
+  {
+    id: "e4-11",
+    source: "4",
+    target: "11",
+    type: "smoothstep",
+    style: { stroke: "black" },
+    markerEnd: {
+      type: MarkerType.ArrowClosed,
+      color: "black",
+    },
+  },
+  {
+    id: "e4-12",
+    source: "4",
+    target: "12",
+    type: "smoothstep",
+    style: { stroke: "black" },
+    markerEnd: {
+      type: MarkerType.ArrowClosed,
+      color: "black",
+    },
+  },
+  {
+    id: "e5-13",
+    source: "5",
+    target: "13",
+    type: "smoothstep",
+    style: { stroke: "black" },
+    markerEnd: {
+      type: MarkerType.ArrowClosed,
+      color: "black",
+    },
+  },
+  {
+    id: "e5-14",
+    source: "5",
+    target: "14",
+    type: "smoothstep",
+    style: { stroke: "black" },
+    markerEnd: {
+      type: MarkerType.ArrowClosed,
+      color: "black",
+    },
+  },
+  {
+    id: "e6-15",
+    source: "6",
+    target: "15",
+    type: "smoothstep",
+    style: { stroke: "black" },
+    markerEnd: {
+      type: MarkerType.ArrowClosed,
+      color: "black",
+    },
+  },
+  {
+    id: "e6-16",
+    source: "6",
+    target: "16",
+    type: "smoothstep",
+    style: { stroke: "black" },
+    markerEnd: {
+      type: MarkerType.ArrowClosed,
+      color: "black",
+    },
+  },
+  {
+    id: "e7-17",
+    source: "7",
+    target: "17",
+    type: "smoothstep",
+    style: { stroke: "black" },
+    markerEnd: {
+      type: MarkerType.ArrowClosed,
+      color: "black",
+    },
+  },
+  {
+    id: "e7-18",
+    source: "7",
+    target: "18",
+    type: "smoothstep",
+    style: { stroke: "black" },
+    markerEnd: {
+      type: MarkerType.ArrowClosed,
+      color: "black",
+    },
+  },
+];
